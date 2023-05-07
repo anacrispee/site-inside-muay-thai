91f41a754216d8fe5c7ca73c7d505e88e8d32172
Ana Cristina <52867914+anacrispee@users.noreply.github.com>
2023-05-07 18:28:02 -0300

    Add files via upload

---
+++ funcoes.js
@@ -0,0 +1,8 @@
+function showMenu() {
+  var x = document.getElementById("nav-list");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
