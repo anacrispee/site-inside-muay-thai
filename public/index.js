d28495988f8580c98548db1722ad54ec3d46fdcd
Ana Cristina <anacrispee@gmail.com>
2023-08-01 12:50:17 -0300

    update ok

--- public/index.js
+++ public/index.js
@@ -15,38 +15,3 @@ function showMenu() {
   };
   
 };
-
-window.addEventListener("DOMContentLoaded", function(){
-
-const botao = document.getElementById('botao');
-botao.addEventListener('click', async function(){
-  
-  const email = document.getElementById('email').value;
-  const telefone = document.getElementById('telefone').value;
-  const mensagem = document.getElementById('mensagem').value;
-
-  const data = {
-    email: email, 
-    telefone: telefone,
-    mensagem: mensagem,
-  }
-
-  fetch('/nodemailer', {
-    method: 'POST',
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(data)
-  })
-  .then(response => response.json())
-  .then(data => {
-    alert("Email enviado com sucesso!");
-  })
-  .catch(error => {
-    console.error("Erro ao enviar email:", error);
-    alert("Erro ao enviar mensagem!");
-  })
-
-});
-
-})
